3dc86070b145823548841561995f443717a05ba4
Beau Hyun Lee <90775148+beauhyunlee@users.noreply.github.com>
2026-05-29 10:26:15 -0400

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded = ""
+        for word in strs:
+            encoded+= str(len(word)) + "#" + word
+        return encoded
+    def decode(self, s: str) -> List[str]:
+        decoded = []
+        i = 0
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            length = int(s[i:j])
+            # increment i to start of the word 
+            i = j + 1 
+            # increment j to end of the word
+            j = length + i
+            # get word
+            decoded.append(s[i:j])
+            # move forward
+            i = j 
+        return decoded
